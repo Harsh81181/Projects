d2fc1efa117c273c5ab3d8220539def2831ea922
Harsh Bhardwaj <78372087+Harsh81181@users.noreply.github.com>
2023-02-25 00:19:05 +0530

    Website Login Automation system

---
+++ auto_login.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+browser=webdriver.Chrome("Enter_webdriver_path")
+browser.maximize_window()
+browser.get("Website_Login_page_link")
+uname=browser.find_element_by_id("Username_or_email")
+pswrd=browser.find_element_by_id("password")
+btn=browser.find_element_by_id("login_btn")
+uname.send_keys("Enter_Your_Username_or_email")
+pswrd.send_keys("Enter_password")
+btn.click()
